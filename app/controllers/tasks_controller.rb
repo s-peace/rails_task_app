4ae145b262d15445eff201e03c17cc10731d85a9
s-peace <ssmim777@gmail.com>
2019-09-21 02:16:09 +0900

    add delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しましたよ-"
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しましたよ-"
+    else
+      render :new
+    end
   end
 
   def edit
