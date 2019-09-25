363a4edc741b20cff237380a62c468ed783a0a97
s-peace <ssmim777@gmail.com>
2019-09-25 16:05:58 +0900

    add rspec 1st test

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # make user A
+      # admin is user a
+    end
+
+    context 'ユーザーＡがログインしているとき' do
+      before do
+        # login as userA
+      end
+
+      it 'ユーザーＡが作成したタスクが表示される' do
+        # view task
+      end
+    end
+  end
+end
